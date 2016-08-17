4121cfb18d300cb6effeaa0d13560e981290f82d
Ruslan Molchanov <ruslanys@gmail.com>
2016-08-18 00:52:13 +0300

    ТРЭШ

--- src/main/java/me/ruslanys/vkaudiosaver/domain/vk/VkResponse.java
+++ src/main/java/me/ruslanys/vkaudiosaver/domain/vk/VkResponse.java
@@ -4,6 +4,10 @@ import lombok.SneakyThrows;
 import me.ruslanys.vkaudiosaver.exceptions.VkException;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 import static org.springframework.util.ReflectionUtils.findField;
@@ -17,14 +21,40 @@ public abstract class VkResponse {
 
     private VkError error;
 
-    @SneakyThrows
     public void setResponse(Map<String, Object> response) {
-        for (Map.Entry<String, Object> entry : response.entrySet()) {
-            Field field = findField(getClass(), entry.getKey());
+        setObject(this, response);
+    }
+
+    @SuppressWarnings("unchecked")
+    @SneakyThrows
+    private void setObject(Object object, Map<String, Object> map) {
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            Field field = findField(object.getClass(), entry.getKey());
             if (field == null) continue;
 
             makeAccessible(field);
-            field.set(this, entry.getValue());
+
+            Class<?> fieldType = field.getType();
+            Type fieldGenericType = field.getGenericType();
+
+            if (List.class.isAssignableFrom(fieldType) && (fieldGenericType instanceof ParameterizedType)) {
+                Type valueType = ((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0];
+
+                ArrayList newList = ArrayList.class.newInstance();
+
+                List<Map<String, Object>> values = ((List<Map<String, Object>>) entry.getValue());
+                for (Map<String, Object> valueMap : values) {
+                    Object newInstance = Class.forName(valueType.getTypeName()).newInstance();
+                    setObject(newInstance, valueMap);
+
+                    newList.add(newInstance);
+                }
+
+                field.set(object, newList);
+            } else {
+                field.set(object, entry.getValue());
+            }
+
         }
     }
 
