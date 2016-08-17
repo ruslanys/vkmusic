b45b4e436b4326f3be4625ff142014bb7cdeb511
Ruslan Molchanov <ruslanys@gmail.com>
2016-08-17 19:42:48 +0300

    .

---
+++ src/main/java/me/ruslanys/vkaudiosaver/domain/Audio.java
@@ -0,0 +1,25 @@
+package me.ruslanys.vkaudiosaver.domain;
+
+import lombok.Data;
+
+/**
+ * @author Ruslan Molchanov (ruslanys@gmail.com)
+ */
+@Data
+public class Audio {
+
+    @Json
+    private Long aid;
+
+/*
+
+"aid": 456239102,
+"owner_id": 22139762,
+"artist": "Сплин",
+"title": "Выхода нет ",
+"duration": 191,
+"url": "http:\/\/cs9-7v4.vk.me\/p3\/57b37421787967.mp3?extra=JY4wiayUm5CVJquT9vedvsDS1cSR9DbwOP3NQmKa8OoUXwEG71D4kGvHtQ8k9rx0Szmq_3ABEykuAhclwn4Fr9i5VSkaF3PGR33KtIt_C0Pi7nfGkzdxrED4pcH1yPOqYi65LpYW9Pg0",
+"genre": 1001
+
+*/
+}
