c4125136e1c00636e12efce26acaeae1220384c8
Ruslan Molchanov <ruslanys@gmail.com>
2016-08-17 18:58:54 +0300

    init

---
+++ README.md
@@ -0,0 +1,17 @@
+Ключ авторизации:
+
+```
+https://oauth.vk.com/authorize?client_id=5592490&redirect_uri=https://oauth.vk.com/blank.html&response_type=token&display=mobile&scope=65544
+```
+
+Access token:
+
+```
+d66836833d100dea3407ea92749ff56547e0e679f1bcc0179040ec5a996f9bff1ca2f8bf85638e85b45ce
+```
+
+Запросы к API:
+
+```
+https://api.vk.com/method/METHOD_NAME?PARAMETERS&access_token=ACCESS_TOKEN&v=V 
+```
---
+++ src/main/java/me/ruslanys/vkaudiosaver/configs/ExecutorsConfig.java
@@ -0,0 +1,54 @@
+package me.ruslanys.vkaudiosaver.configs;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Date: 02.02.16
+ * Time: 14:40
+ *
+ * @author Ruslan Molchanov (ruslanys@gmail.com)
+ * @author http://mruslan.com
+ */
+@Configuration
+@EnableScheduling
+@EnableAsync
+public class ExecutorsConfig implements SchedulingConfigurer, AsyncConfigurer {
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        ThreadPoolTaskScheduler taskScheduler = new ThreadPoolTaskScheduler();
+        taskScheduler.setPoolSize(5);
+        taskScheduler.initialize();
+        taskScheduler.setThreadNamePrefix("ScheduledExecutor-");
+
+        taskRegistrar.setTaskScheduler(taskScheduler);
+    }
+
+    @Bean(name = "asyncExecutor")
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setThreadNamePrefix("AsyncExecutor-");
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        return (throwable, method, objects) -> LoggerFactory.getLogger("Async").error("Async error", throwable); // ?
+    }
+
+}
