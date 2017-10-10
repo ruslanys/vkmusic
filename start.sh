89d1b010235fc27c4800fdc6d78746c58b21ca97
Ruslan Molchanov <ruslanys@gmail.com>
2017-10-10 18:55:57 +0300

    Don't need start.sh anymore

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Downloading..."
-sleep 2
-java -jar vkaudiosaver-1.1.jar -type scraper -u $1 -p $2 --pool-size 5 /home/ruslanys/vkaudiosaver/Music
-
-echo "Updating mp3 tags"
-sleep 2
-find . -iname "*.mp3" -execdir mid3iconv -e "windows-1251" {} \;
