fd0d2b034169ffd21dbb703fb13dc068e5170685
Ruslan Molchanov <ruslanys@gmail.com>
2018-08-27 00:41:24 +0300

    Properties

---
+++ src/main/kotlin/me/ruslanys/vkmusic/property/DownloaderProperties.kt
@@ -0,0 +1,6 @@
+package me.ruslanys.vkmusic.property
+
+/**
+ * @author Ruslan Molchanov (ruslanys@gmail.com)
+ */
+class DownloaderProperties(val poolSize: Int = 5) : Properties()
---
+++ src/main/kotlin/me/ruslanys/vkmusic/property/VkCookies.kt
@@ -0,0 +1,6 @@
+package me.ruslanys.vkmusic.property
+
+/**
+ * @author Ruslan Molchanov (ruslanys@gmail.com)
+ */
+class VkCookies(val sessionId: String) : Properties()
