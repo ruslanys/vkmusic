3073b5958b036c0bd5fd0bec91f2a1344c0858d6
Ruslan Molchanov <ruslanys@gmail.com>
2018-08-27 11:07:47 +0300

    ExecutorsConfig

---
+++ src/main/kotlin/me/ruslanys/vkmusic/config/ExecutorsConfig.kt
@@ -0,0 +1,43 @@
+package me.ruslanys.vkmusic.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.AsyncConfigurer
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.SchedulingConfigurer
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+import org.springframework.scheduling.config.ScheduledTaskRegistrar
+import java.util.concurrent.Executor
+
+@EnableScheduling
+@EnableAsync
+@Configuration
+class ExecutorsConfig : SchedulingConfigurer, AsyncConfigurer {
+
+    override fun configureTasks(taskRegistrar: ScheduledTaskRegistrar) {
+        val taskScheduler = ThreadPoolTaskScheduler()
+        taskScheduler.poolSize = 5
+        taskScheduler.setThreadNamePrefix("ScheduledExecutor-")
+        taskScheduler.initialize()
+
+        taskRegistrar.setTaskScheduler(taskScheduler)
+    }
+
+    override fun getAsyncExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 5
+        executor.setThreadNamePrefix("AsyncExecutor-")
+        executor.initialize()
+
+        return executor
+    }
+
+    override fun getAsyncUncaughtExceptionHandler(): AsyncUncaughtExceptionHandler =
+            AsyncUncaughtExceptionHandler { throwable, _, _ ->
+                LoggerFactory.getLogger("Async").error("Async error", throwable)
+            }
+
+}
