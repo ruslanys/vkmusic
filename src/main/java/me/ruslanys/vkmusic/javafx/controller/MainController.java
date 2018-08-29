79f6cbcecbe2e96032fbdb0dc70eaba6286e76c6
Ruslan Molchanov <ruslanys@gmail.com>
2018-08-30 01:41:05 +0300

    Main scene: Remove menu bar

--- src/main/java/me/ruslanys/vkmusic/javafx/controller/MainController.java
+++ src/main/java/me/ruslanys/vkmusic/javafx/controller/MainController.java
@@ -39,7 +39,7 @@ public class MainController {
     }
 
     private void initLoadingIcon() {
-        loadingImageView.setImage(IconUtils.getLoadingIcon());
+        loadingImageView.setImage(IconUtils.INSTANCE.getLoadingIcon());
     }
 
     private void initTableView() {
