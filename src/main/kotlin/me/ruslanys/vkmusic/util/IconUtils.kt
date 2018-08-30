0e3337834f599c59fee92f29b40d18da042fcf0d
Ruslan Molchanov <ruslanys@gmail.com>
2018-08-30 16:15:19 +0300

    Icons

--- src/main/kotlin/me/ruslanys/vkmusic/util/IconUtils.kt
+++ src/main/kotlin/me/ruslanys/vkmusic/util/IconUtils.kt
@@ -1,17 +1,33 @@
 package me.ruslanys.vkmusic.util
 
 import javafx.scene.image.Image
+import me.ruslanys.vkmusic.domain.DownloadStatus
+import java.util.*
 
 object IconUtils {
 
-    fun getLoadingIcon(): Image {
-        val resource = IconUtils::class.java.classLoader.getResourceAsStream("images/loading-heart.gif")
-        return Image(resource)
+    private val desktopIcon: Image
+    private val loadingIcon: Image
+    private val statusIcon: Map<DownloadStatus, Image>
+
+    init {
+        desktopIcon = loadImage("images/icon.png")
+        loadingIcon = loadImage("images/loading-heart.gif")
+        statusIcon = EnumMap<DownloadStatus, Image>(DownloadStatus::class.java)
+        for (status in DownloadStatus.values()) {
+            val imageView = loadImage("images/status/${status.name.toLowerCase()}.png")
+            statusIcon[status] = imageView
+        }
     }
 
-    fun getDesktopIcon(): Image {
-        val resource = IconUtils::class.java.classLoader.getResourceAsStream("images/icon.png")
-        return Image(resource)
+    fun getLoadingIcon(): Image = loadingIcon
+
+    fun getStatusIcon(status: DownloadStatus): Image = statusIcon[status]!!
+
+    fun getDesktopIcon(): Image = desktopIcon
+
+    private fun loadImage(path: String): Image {
+        return Image(IconUtils::class.java.classLoader.getResourceAsStream(path))
     }
 
 }
