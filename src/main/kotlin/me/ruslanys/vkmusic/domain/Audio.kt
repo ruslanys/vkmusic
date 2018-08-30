2c27d51a82dc254cf8996094edfbd616ce52d8ea
Ruslan Molchanov <ruslanys@gmail.com>
2018-08-30 13:32:22 +0300

    Audio reduced

--- src/main/kotlin/me/ruslanys/vkmusic/domain/Audio.kt
+++ src/main/kotlin/me/ruslanys/vkmusic/domain/Audio.kt
@@ -6,7 +6,6 @@ data class Audio(
         var artist: String,
         var title: String,
         var duration: Int,
-        var position: Int? = null,
         var status: DownloadStatus = DownloadStatus.NEW,
         var url: String? = null
 )
