f401527c02740e476bd3a33e5785095d2e860d86
Ruslan Molchanov <ruslanys@gmail.com>
2018-08-30 14:19:09 +0300

    MainController#downloadSelected

--- src/main/kotlin/me/ruslanys/vkmusic/ui/controller/MainController.kt
+++ src/main/kotlin/me/ruslanys/vkmusic/ui/controller/MainController.kt
@@ -12,11 +12,21 @@ import javafx.scene.layout.Pane
 import me.ruslanys.vkmusic.annotation.FxmlController
 import me.ruslanys.vkmusic.component.VkClient
 import me.ruslanys.vkmusic.domain.Audio
+import me.ruslanys.vkmusic.domain.DownloadStatus
+import me.ruslanys.vkmusic.event.DownloadEvent
+import me.ruslanys.vkmusic.event.DownloadFailEvent
+import me.ruslanys.vkmusic.event.DownloadInProgressEvent
+import me.ruslanys.vkmusic.event.DownloadSuccessEvent
+import me.ruslanys.vkmusic.service.DownloadService
 import me.ruslanys.vkmusic.util.IconUtils
+import org.slf4j.LoggerFactory
+import org.springframework.context.ApplicationListener
 import java.util.concurrent.CompletableFuture
 
 @FxmlController(view = "views/main.fxml")
-class MainController(private val vkClient: VkClient) : BaseController() {
+class MainController(
+        private val vkClient: VkClient,
+        private val downloadService: DownloadService) : ApplicationListener<DownloadEvent>, BaseController() {
 
     @FXML private lateinit var loadingView: Pane
     @FXML private lateinit var loadingImageView: ImageView
@@ -82,6 +92,32 @@ class MainController(private val vkClient: VkClient) : BaseController() {
             data.addAll(it)
             tableView.isVisible = true
         }
+    }
+
+    @FXML
+    fun downloadSelected() {
+        tableView.selectionModel.selectedItems.apply {
+            forEach { it.status = DownloadStatus.QUEUED }
+            tableView.refresh()
+
+            downloadService.download("/Users/ruslanys/Downloads/music", this.toList())
+        }
+    }
+
+    override fun onApplicationEvent(event: DownloadEvent) {
+        log.info("Event $event")
+
+        when (event) {
+            is DownloadSuccessEvent -> event.audio.status = DownloadStatus.SUCCESS
+            is DownloadFailEvent -> event.audio.status = DownloadStatus.FAIL
+            is DownloadInProgressEvent -> event.audio.status = DownloadStatus.IN_PROGRESS
+        }
+
+        tableView.refresh()
+    }
+
+    companion object {
+        private val log = LoggerFactory.getLogger(MainController::class.java)
     }
 
 }
